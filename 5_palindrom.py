8dbf9b3d091f1dc5861875f9e638c65618449f7e
Pavan Shirsath <pavanshir60@gmail.com>
2026-02-13 22:25:05 +0530

    Solved Day 2 palindrom number

---
+++ 5_palindrom.py
@@ -0,0 +1,16 @@
+# Palindrome Number
+# Given an integer x, return true if x is a palindrome, and false otherwise.
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        temp = x
+        rev = 0
+
+        while temp>0:
+            r = temp%10
+            temp//=10
+            rev= rev*10 + r
+
+        return  rev==x 
+        
+        
